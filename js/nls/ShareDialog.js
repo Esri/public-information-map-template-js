8186549457018d4fcddf26938c601480b6e23f53
Matt Driscoll <matthew.driscoll@gmail.com>
2014-01-07 21:43:34 -0800

    no message

--- js/nls/ShareDialog.js
+++ js/nls/ShareDialog.js
@@ -4,9 +4,9 @@ define({
             "ShareDialog": {
                 "title": "Share",
                 "heading": "Share this map",
-                "url": "Map URL",
+                "url": "Map Link",
                 "embed": "Embed Map",
-                "size": "Map size (width/height):",
+                "size": "Size (width/height):",
                 "facebookTooltip": "Facebook",
                 "twitterTooltip": "Twitter",
                 "gplusTooltip": "Google Plus",
