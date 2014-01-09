d010440d4a09a30f2299724b6a19410e76d52a60
Matt Driscoll <matthew.driscoll@gmail.com>
2014-01-08 18:41:14 -0800

    no message

--- resources/configurationPanel.js
+++ resources/configurationPanel.js
@@ -4,29 +4,148 @@
          "category":"<b>Choose template theme</b>",
          "fields":[
             {
-               "placeHolder":"Defaults to map owner",
-               "label":"Owner Text:",
-               "fieldName":"owner",
-               "type":"string",
-               "tooltip":"Defaults to map owner"
+               "type":"webmap",
+               "label":"Select a map"
+            }
+         ]
+      },
+       {
+         "category":"<b>Webcams Options</b>",
+         "fields":[
+            {
+               "type":"boolean",
+               "fieldName":"showWebcams",
+               "label":"Show Webcams",
+               "tooltip":"Show Webcams"
+            },
+            {
+               "type":"paragraph",
+               "value":"Show Webcams on this template."
+            },
+            {
+               "type":"boolean",
+               "fieldName":"webcamsChecked",
+               "label":"Checked",
+               "tooltip":"Checked"
+            },
+            {
+               "type":"paragraph",
+               "value":"Turn this layer on by default."
+            }
+         ]
+      },
+       {
+         "category":"<b>Instagram Options</b>",
+         "fields":[
+            {
+               "type":"boolean",
+               "fieldName":"showInstagram",
+               "label":"Show Instagram",
+               "tooltip":"Show Instagram"
+            },
+            {
+               "type":"paragraph",
+               "value":"Show Instagram on this template."
+            },
+            {
+               "type":"boolean",
+               "fieldName":"instagramChecked",
+               "label":"Checked",
+               "tooltip":"Checked"
+            },
+            {
+               "type":"paragraph",
+               "value":"Turn this layer on by default."
+            }
+         ]
+      },
+      {
+         "category":"<b>Twitter Options</b>",
+         "fields":[
+            {
+               "type":"boolean",
+               "fieldName":"showTwitter",
+               "label":"Show Twitter",
+               "tooltip":"Show Twitter"
+            },
+            {
+               "type":"paragraph",
+               "value":"Show Twitter on this template."
+            },
+            {
+               "type":"boolean",
+               "fieldName":"twitterChecked",
+               "label":"Checked",
+               "tooltip":"Checked"
+            },
+            {
+               "type":"paragraph",
+               "value":"Turn this layer on by default."
             },
             {
                "type":"string",
-               "fieldName":"theme",
-               "tooltip":"Color theme to use",
-               "label":"Color Scheme:",
+               "fieldName":"twitterSearch",
+               "label":"Search Keywords",
+               "tooltip":"Search Keywords",
+               "placeHolder":""
+            },
+            {
+               "type":"paragraph",
+               "value":"<a href=\"http://support.twitter.com/articles/71577-how-to-use-advanced-twitter-search\" target=\"_blank\">Advanced search</a>."
+            }
+         ]
+      },
+      {
+         "category":"<b>Flickr Options</b>",
+         "fields":[
+            {
+               "type":"boolean",
+               "fieldName":"showFlickr",
+               "label":"Show Flickr",
+               "tooltip":"Show Flickr"
+            },
+            {
+               "type":"paragraph",
+               "value":"Show Flickr on this template."
+            },
+            {
+               "type":"boolean",
+               "fieldName":"flickrChecked",
+               "label":"Checked",
+               "tooltip":"Checked"
+            },
+            {
+               "type":"paragraph",
+               "value":"Turn this layer on by default."
+            },
+            {
+               "type":"string",
+               "fieldName":"flickrSearch",
+               "label":"Search Keywords",
+               "tooltip":"Search Keywords",
+               "placeHolder":""
+            },
+            {
+               "type":"string",
+               "fieldName":"flickrRange",
+               "tooltip":"Date Range",
+               "label":"Date Range:",
                "options":[
                   {
-                     "label":"Chrome",
-                     "value":"chrome"
+                     "label":"Today",
+                     "value":"today"
                   },
                   {
-                     "label":"Seaside",
-                     "value":"seaside"
+                     "label":"This Week",
+                     "value":"this_week"
                   },
                   {
-                     "label":"Pavement",
-                     "value":"pavement"
+                     "label":"This Month",
+                     "value":"this_month"
+                  },
+                  {
+                     "label":"All Time",
+                     "value":"all_time"
                   }
                ]
             }
@@ -34,6 +153,15 @@
       }
    ],
    "values":{
-
+        "title":"",
+        "showTitle":true,
+        "showLegend": true,
+        "showHomeButton": true,
+        "showLocateButton":true,
+        "showBasemapToggle": true,
+        "showAboutDialog":true,
+        "ShowShareDialog":true,
+        "nextBasemap": "hybrid",
+        "currentBasemap": "topo"
    }
 }
