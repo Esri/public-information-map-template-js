13615345a95dcf295fa86f9360f0f88b0341ce7f
Matt Driscoll <mdriscoll@esri.com>
2014-02-28 10:43:48 -0800

    config defaults

--- config/defaults.js
+++ config/defaults.js
@@ -58,7 +58,26 @@ define([], function() {
         //Enter the url to your organizations bing maps key if you want to use bing basemaps
         "bingmapskey": "",
         //Defaults to arcgis.com. Set this value to your portal or organization host name.
-        "sharinghost": location.protocol + "//" + "www.arcgis.com"
+        "sharinghost": location.protocol + "//" + "www.arcgis.com",
+        //When true the template will query arcgis.com for default settings for helper services, units etc. If you 
+        //want to use custom settings for units or any of the helper services set queryForOrg to false then enter
+        //default values for any items you need using the helper services and units properties. 
+        "queryForOrg": false, 
+        "units": null, 
+        "helperServices": {  
+           "geometry":{
+            "url": null
+           },
+           "printTask": {
+            "url": null
+           },
+           "elevationSync":{
+             "url": null
+           },
+           "geocode": [{
+            "url": null
+           }]
+        }
     };
     return defaults;
 });
