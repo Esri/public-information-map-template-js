9c7f1fb1629446152b7eb1825b0cdb3304f91ae9
Matt Driscoll <mdriscoll@esri.com>
2014-03-21 09:22:09 -0700

    no message

--- config/defaults.js
+++ config/defaults.js
@@ -61,7 +61,7 @@ define({
     //When true the template will query arcgis.com for default settings for helper services, units etc. If you 
     //want to use custom settings for units or any of the helper services set queryForOrg to false then enter
     //default values for any items you need using the helper services and units properties. 
-    "queryForOrg": false,
+    "queryForOrg": true,
     "localize": true,
     "urlItems": [
         "extent"
