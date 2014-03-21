2fc8da56d554d79653b21579f5067567d0b19da0
Matt Driscoll <mdriscoll@esri.com>
2014-03-21 15:30:47 -0700

    fix bitly for extent

--- js/ShareDialog.js
+++ js/ShareDialog.js
@@ -109,7 +109,7 @@ define([
                 this.watch("embedSizes", this._setSizeOptions);
                 this.watch("embed", this._updateEmbed);
                 this.watch("bitlyUrl", this._updateBitlyUrl);
-                this.watch("useExtent", this._updateUrl);
+                this.watch("useExtent", this._useExtentChanged);
                 // classes
                 this.css = {
                     container: "button-container",
@@ -197,6 +197,10 @@ define([
             _useExtentUpdate: function () {
                 var value = domAttr.get(this._extentInput, 'checked');
                 this.set("useExtent", value);
+            },
+            _useExtentChanged: function(){
+                this._updateUrl();
+                this._shareLink();  
             },
             _removeEvents: function () {
                 if (this._events && this._events.length) {
