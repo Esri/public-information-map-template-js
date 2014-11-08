7130854b81b373fbc2dece7dbddc336ce12eaca7
tomoyo812 <moyo812.tyys911gaf20@gmail.com>
2014-11-08 14:16:43 +0900

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -3,7 +3,7 @@ define({
     //default web map, default app color theme and more. These values can be overwritten by template configuration settings
     //and url parameters.
     "appid": "",
-    "webmap": "df8bcc10430f48878b01c96e907a1fc3",
+    "webmap": "a2fb7c4258194d38b24ed96a551bc032",
     "oauthappid": null,
     //Enter the url to the proxy if needed by the applcation. See the "Using the proxy page" help topic for details
     //developers.arcgis.com/en/javascript/jshelp/ags_proxy.html
