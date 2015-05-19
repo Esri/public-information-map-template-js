13886106fdeef7dd096390548d7550aba346dec2
Matt Driscoll <MDriscoll@esri.com>
2015-05-19 09:16:03 -0700

    update boilerplate

--- js/template.js
+++ js/template.js
@@ -1,6 +1,6 @@
 /*
-  Version 1.4
-  2/3/2015
+  Version 1.5
+  5/19/2015
 */
 
 /*global define,document,location,require */
@@ -20,26 +20,42 @@
  | See the License for the specific language governing permissions and
  | limitations under the License.
  */
-define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/kernel", "dojo/_base/array", "dojo/_base/lang", "dojo/dom-class", "dojo/Deferred", "dojo/promise/all", "esri/arcgis/utils", "esri/urlUtils", "esri/request", "esri/config", "esri/lang", "esri/IdentityManager", "esri/arcgis/Portal", "esri/arcgis/OAuthInfo", "esri/tasks/GeometryService", "config/defaults", "dojo/string"], function (
-  Evented,
-  declare,
-  kernel,
-  array,
-  lang,
+define([
+  "dojo/_base/array",
+  "dojo/_base/declare",
+  "dojo/_base/kernel",
+  "dojo/_base/lang",
+
+  "dojo/Evented",
+  "dojo/Deferred",
+  "dojo/string",
+
+  "dojo/dom-class",
+
+  "dojo/promise/all",
+
+  "esri/config",
+  "esri/IdentityManager",
+  "esri/lang",
+  "esri/request",
+  "esri/urlUtils",
+
+  "esri/arcgis/Portal",
+  "esri/arcgis/OAuthInfo",
+  "esri/arcgis/utils",
+
+  "esri/tasks/GeometryService",
+
+  "config/defaults"
+], function (
+  array, declare, kernel, lang,
+  Evented, Deferred, string,
   domClass,
-  Deferred,
   all,
-  arcgisUtils,
-  urlUtils,
-  esriRequest,
-  esriConfig,
-  esriLang,
-  IdentityManager,
-  esriPortal,
-  ArcGISOAuthInfo,
+  esriConfig, IdentityManager, esriLang, esriRequest, urlUtils,
+  esriPortal, ArcGISOAuthInfo, arcgisUtils,
   GeometryService,
-  defaults,
-  string
+  defaults
 ) {
   return declare([Evented], {
     config: {},
@@ -389,12 +405,28 @@ define(["dojo/Evented", "dojo/_base/declare", "dojo/_base/kernel", "dojo/_base/a
             this.itemConfig = cfg;
             deferred.resolve(cfg);
           }));
-        } else {
-          // if webmap does not exist
-          if (!this.config.webmap) {
-            // use default webmap for boilerplate
-            this.config.webmap = "24e01ef45d40423f95300ad2abc5038a";
-          }
+        }
+        // no webmap is set and we have organization's info
+        else if (!this.config.webmap && this.config.orgInfo) {
+          var defaultWebmap = {
+            "item": {
+              "title": "Default Webmap",
+              "type": "Web Map",
+              "description": "A webmap with the default basemap and extent.",
+              "snippet": "A webmap with the default basemap and extent.",
+              "extent": this.config.orgInfo.defaultExtent
+            },
+            "itemData": {
+              "operationalLayers": [],
+              "baseMap": this.config.orgInfo.defaultBasemap
+            }
+          };
+          cfg.itemInfo = defaultWebmap;
+          this.itemConfig = cfg;
+          deferred.resolve(cfg);
+        }
+        // use webmap from id
+        else {
           arcgisUtils.getItem(this.config.webmap).then(lang.hitch(this, function (itemInfo) {
             // Set the itemInfo config option. This can be used when calling createMap instead of the webmap id
             cfg.itemInfo = itemInfo;
