ba28d3510550fc3d64066dc30a1d8b8708c01c0f
Matt Driscoll <MDriscoll@esri.com>
2015-07-06 15:18:21 -0700

    new pim sample

--- config/defaults.js
+++ config/defaults.js
@@ -3,7 +3,7 @@ define({
   //default web map, default app color theme and more. These values can be overwritten by template configuration settings
   //and url parameters.
   "appid": "",
-  "webmap": "df8bcc10430f48878b01c96e907a1fc3",
+  "webmap": "4b762458c7994fc08b4994a2dc76e9fc",
   "oauthappid": null,
   //Enter the url to the proxy if needed by the applcation. See the "Using the proxy page" help topic for details
   //developers.arcgis.com/en/javascript/jshelp/ags_proxy.html
